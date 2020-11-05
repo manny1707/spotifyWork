7fc4569d64205e8a0728a3fc38b76d1e95be884a
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-05 01:07:41 -0600

    plz help

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -3,15 +3,18 @@ var codeForAuth = null
 function spotify() {
     codeForAuth = window.location.search.substr(6, window.location.search.length-6);
     let redirect = "https%3A%2F%2Fmanny1707.github.io%2FspotifyWork"
-    const data = { grant_type : "authorization_code", code: codeForAuth, redirect_uri: redirect};
+    const params = { grant_type : "authorization_code", code: codeForAuth, redirect_uri: redirect};
 
     var encoded = "NzcxNTk4OTEwNGY5NDgxYmI3MDllYjQyODIyMjkwZTE6NGM5ZDg5MDhlZDJiNDlkODliYjdiN2M4OTBkODgxNjY="
 
 
     let apiRequest = {
         method: 'POST',
-        headers: {'Authorization': 'Authorization: Basic ' + encoded},
-        body: JSON.stringify(data),
+        headers: {
+            'Content-type' : "application/x-www-form-urlencoded",
+            'Authorization': 'Authorization: Basic ' + encoded
+        },
+        body: JSON.stringify(params),
     };
 
     fetch(`https://accounts.spotify.com/api/token`, apiRequest)
@@ -19,6 +22,8 @@ function spotify() {
             console.log(apiResponse)
         })
         .catch(error => console.log(error));
+
+    
     /*fetch(`https://cors-anywhere.herokuapp.com/accounts.spotify.com/authorize?client_id=7715989104f9481bb709eb42822290e1&response_type=code&redirect_uri=https%3A%2F%2Fwww.exabilityapp.com&scope=user-read-private%20user-read-email&state=34fFs29kd09`, apiRequest)
         .then(apiResponse => {
             console.log(apiResponse)
