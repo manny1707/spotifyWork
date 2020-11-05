f7d27d5c91669dc7d0cd97e58c4cab498e239073
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-05 00:56:49 -0600

    help plz

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -1,75 +1,30 @@
-/*$( document ).ready(function() {
-    console.log( 'script.js ready!' );
-    console.log('version 6')
-
-    const getUrlParameter = (sParam) => {
-      let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
-          sURLVariables = sPageURL.split('#'),
-          sParameterName,
-          i;
-      let split_str = window.location.href.split('#');
-      sURLVariables = split_str[1].split('&');
-      for (i = 0; i < sURLVariables.length; i++) {
-          sParameterName = sURLVariables[i].split('=');
-          if (sParameterName[0] === sParam) {
-              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-          }
-      }
-  };
-
-    const accessToken = getUrlParameter('access_token');
-    console.log(`accessToken ${accessToken}`);
-
-  $.ajax ({
-      url: 'https://api.spotify.com/v1/search?q=dance+off&type=track',
-      type: 'GET',
-      headers: {
-          'Authorization' : 'Bearer ' + accessToken
-      },
-      success: function(data) {
-        console.log(' ');
-        console.log(' ');
-        console.log('Got data back');
-        // Let's console what gets returned for our search
-        console.log(JSON.stringify(data));
-        // Example: Extract the id of the song from the data object
-        let id = data.tracks.items[0].id;
-        console.log(' ');
-        console.log(`id ${id}`); ////id 1TEL6MlSSVLSdhOSddidlJ
-        // Constructing a iframe to embed a song
-        let src_str = `https://open.spotify.com/embed/track/${id}`;
-        console.log(`src_str ${src_str}`);
-        let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
-        let parent_div = $('#content');
-        parent_div.append(iframe);
-      }
-  });
-
-});*/
+var codeForAuth = null
 
 function spotify() {
-    console.log(window.location.search.substr(6, window.location.search.length-6));
-}
+    codeForAuth = window.location.search.substr(6, window.location.search.length-6);
+    let redirect = "https%3A%2F%2Fmanny1707.github.io%2FspotifyWork"
+    const data = { grant_type : "authorization_code", code: codeForAuth, redirect_uri: redirect};
 
-spotify();
-/*async function getspotify() {
-    console.log("apiResponse")
-    let apiHeaders = new Headers();
-    let apiBody = null;
+    var encoded = "NzcxNTk4OTEwNGY5NDgxYmI3MDllYjQyODIyMjkwZTE6NGM5ZDg5MDhlZDJiNDlkODliYjdiN2M4OTBkODgxNjY="
 
-    apiHeaders.append('Content-Type', 'application/json');
 
     let apiRequest = {
-        method: 'GET',
-        headers: apiHeaders,
-        redirect: 'follow'
+        method: 'POST',
+        headers: {'Authorization': 'Authorization: Basic ' + encoded},
+        body: JSON.stringify(data),
     };
 
-    fetch(`https://cors-anywhere.herokuapp.com/accounts.spotify.com/authorize?client_id=7715989104f9481bb709eb42822290e1&response_type=code&redirect_uri=https%3A%2F%2Fwww.exabilityapp.com&scope=user-read-private%20user-read-email&state=34fFs29kd09`, apiRequest)
+    fetch(`https://accounts.spotify.com/api/token`, apiRequest)
         .then(apiResponse => {
             console.log(apiResponse)
         })
         .catch(error => console.log(error));
+    /*fetch(`https://cors-anywhere.herokuapp.com/accounts.spotify.com/authorize?client_id=7715989104f9481bb709eb42822290e1&response_type=code&redirect_uri=https%3A%2F%2Fwww.exabilityapp.com&scope=user-read-private%20user-read-email&state=34fFs29kd09`, apiRequest)
+        .then(apiResponse => {
+            console.log(apiResponse)
+        })
+        .catch(error => console.log(error));*/
 }
 
-getspotify();*/
+
+spotify();
