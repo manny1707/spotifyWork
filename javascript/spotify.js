40d2063cae93a8ba0f2bf9a84b59667e5eb43125
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-04 20:01:21 -0600

    log in

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -1,5 +1,5 @@
-$( document ).ready( function (){
-    console.log( 'spotify.js ready!' );
+$( document ).ready(function() {
+    console.log( 'script.js ready!' );
     console.log('version 6')
 
     const getUrlParameter = (sParam) => {
@@ -17,12 +17,35 @@ $( document ).ready( function (){
       }
   };
 
-    const accessToken = getUrlParameter('code');
+    const accessToken = getUrlParameter('access_token');
     console.log(`accessToken ${accessToken}`);
 
+  $.ajax ({
+      url: 'https://api.spotify.com/v1/search?q=dance+off&type=track',
+      type: 'GET',
+      headers: {
+          'Authorization' : 'Bearer ' + accessToken
+      },
+      success: function(data) {
+        console.log(' ');
+        console.log(' ');
+        console.log('Got data back');
+        // Let's console what gets returned for our search
+        console.log(JSON.stringify(data));
+        // Example: Extract the id of the song from the data object
+        let id = data.tracks.items[0].id;
+        console.log(' ');
+        console.log(`id ${id}`); ////id 1TEL6MlSSVLSdhOSddidlJ
+        // Constructing a iframe to embed a song
+        let src_str = `https://open.spotify.com/embed/track/${id}`;
+        console.log(`src_str ${src_str}`);
+        let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+        let parent_div = $('#content');
+        parent_div.append(iframe);
+      }
+  });
+
 });
-
-
 
 /*async function getspotify() {
     console.log("apiResponse")
