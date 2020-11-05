4308090b6a8e1a9b86e26c7268777576f31ad2b0
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-04 19:37:34 -0600

    neweww

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -1,4 +1,30 @@
-async function getspotify() {
+$( document ).ready( function (){
+    console.log( 'script.js ready!' );
+    console.log('version 6')
+
+    const getUrlParameter = (sParam) => {
+      let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
+          sURLVariables = sPageURL.split('#'),
+          sParameterName,
+          i;
+      let split_str = window.location.href.split('#');
+      sURLVariables = split_str[1].split('&');
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+          }
+      }
+  };
+
+    const accessToken = getUrlParameter('access_token');
+    console.log(`accessToken ${accessToken}`);
+
+});
+
+
+
+/*async function getspotify() {
     console.log("apiResponse")
     let apiHeaders = new Headers();
     let apiBody = null;
@@ -18,4 +44,4 @@ async function getspotify() {
         .catch(error => console.log(error));
 }
 
-getspotify();
+getspotify();*/
