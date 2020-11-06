5684adba3c0e826919faee94620c99bc610bcb73
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-06 01:08:09 -0600

    fk

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -6,39 +6,19 @@ function spotify() {
 
     accessToken = hash.substring(13, accessString);
     console.log("Access Token: " + accessToken);
-    //console.log(codeForAuth);
     let redirect = "https%3A%2F%2Fmanny1707.github.io%2FspotifyWork";
-    let params = { 
-        "grant_type" : "authorization_code", 
-        "code" : codeForAuth, 
-        "redirect_uri" : redirect
-    };
 
     var encoded = "NzcxNTk4OTEwNGY5NDgxYmI3MDllYjQyODIyMjkwZTE6NGM5ZDg5MDhlZDJiNDlkODliYjdiN2M4OTBkODgxNjY="
 
     
-    $.ajax({
-        url: 'https://cors-anywhere.herokuapp.com/api.spotify.com/v1/me',
+    let apiRequest = {
+        method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + accessToken
         },
-        success: function(response) {
-            console.log(response)
-        }
-    })
-
-    /*
-    let apiRequest = {
-        method: 'POST',
-        headers: {
-            'Content-Type' : "application/x-www-form-urlencoded",
-            'Authorization': 'Basic ' + encoded
-        },
-        body: params,
-        JSON: true
     };
 
-    fetch(`https://cors-anywhere.herokuapp.com/accounts.spotify.com/api/token`, apiRequest)
+    fetch(`https://cors-anywhere.herokuapp.com/api.spotify.com/v1/me/player/currently-playing`, apiRequest)
         .then(apiResponse => {
             console.log(apiResponse)
         })
