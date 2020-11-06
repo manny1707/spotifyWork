784d46729d2b464bbb379f2f23604e82593d6ffb
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-05 23:44:11 -0600

    ha

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -22,7 +22,7 @@ function spotify() {
         body: JSON.stringify(params)
     };
 
-    fetch(`http://cors-anywhere.herokuapp.com/accounts.spotify.com/api/token`, apiRequest)
+    fetch(`https://cors-anywhere.herokuapp.com/accounts.spotify.com/api/token`, apiRequest)
         .then(apiResponse => {
             console.log(apiResponse)
         })
