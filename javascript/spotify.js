436120dc56d5fbaa284c0f38a17181920fe4ece6
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-06 23:07:23 -0600

    lmoa

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -18,7 +18,7 @@ function spotify() {
         },
     };*/
 
-    const result = await fetch(`https://cors-anywhere.herokuapp.com/api.spotify.com/v1/me/player/currently-playing`, {
+    const result = fetch(`https://cors-anywhere.herokuapp.com/api.spotify.com/v1/me/player/currently-playing`, {
         method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + accessToken
