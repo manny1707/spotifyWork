7ea4faffe14e5741712e49e22ade7a1b38dfc5b4
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-07 22:11:30 -0600

    lmoa

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -14,6 +14,8 @@ function spotify() {
     let apiRequest = {
         method: 'GET',
         headers: {
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true, 
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + accessToken
