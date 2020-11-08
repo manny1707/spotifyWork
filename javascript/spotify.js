df25e5465f51ce1e9cdd7912b139fba5faec6cb9
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-07 22:57:32 -0600

    lmoa

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -22,9 +22,7 @@ async function spotify() {
 
     await fetch(`https://cors-anywhere.herokuapp.com/api.spotify.com/v1/me/player/currently-playing?market=US`, apiRequest)
         .then(apiResponse => {
-            console.log(apiResponse.json().then(
-                data => { console.log(data) }
-            ));
+            console.log(apiResponse);
         })
         .catch(error => console.log(error));
 
