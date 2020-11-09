4181b4d41387d77f9792310a4bd358a73d23107d
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-09 00:28:28 -0600

    lol

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -1,7 +1,7 @@
 var accessToken = null
 
 function Redirect() {
-    window.location.replace = 'https://accounts.spotify.com/authorize?client_id=7715989104f9481bb709eb42822290e1&redirect_uri=https%3A%2F%2Fmanny1707.github.io%2FspotifyWork&scope=user-read-currently-playing%20user-read-email&response_type=token';
+    window.location = 'https://accounts.spotify.com/authorize?client_id=7715989104f9481bb709eb42822290e1&redirect_uri=https%3A%2F%2Fmanny1707.github.io%2FspotifyWork&scope=user-read-currently-playing%20user-read-email&response_type=token';
 }
 
 async function spotify() {
