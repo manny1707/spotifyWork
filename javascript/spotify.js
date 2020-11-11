8ff7a2fb5ea0c1f38376a9cdab01e1be2ced8fb4
Manuel Marroquin <manuelmarroquin1707@gmail.com>
2020-11-11 01:20:19 -0600

    lmao

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -86,7 +86,7 @@ async function pauseSong(){
         .catch(error => console.log(error));*/    
 }
 
-if (ReadCookie("Access_Token").length == 0){
+if (document.cookie.indexOf("Access_Token=") < 0){
     var hash = window.location.hash.substring(1);
     var accessString = hash.indexOf("&");
     accessToken = hash.substring(13, accessString);
